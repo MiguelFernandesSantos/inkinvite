cf8d8c37d13ca6e4b261b42b3bbe519a48da07c4
Miguel <miguel.fernandes@hipcom.com.br>
2024-05-14 19:48:09 -0300

    Alterando migration para primeiro criar a tabela de usuarios e depois a de obra e adicionando novas constraints.

--- src/main/resources/db/scripts/V01__schema.sql
+++ src/main/resources/db/scripts/V01__schema.sql
@@ -1,15 +1,6 @@
 -- liquibase formatted sql
 -- changeset inkinvite:V01__schema splitStatements:true endDelimiter:;
 
-CREATE TABLE IF NOT EXISTS obra (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    autor BIGINT NOT NULL,
-    titulo VARCHAR(255) NOT NULL,
-    descricao TEXT,
-    status INT NOT NULL,
-    data_criacao TIMESTAMP NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS usuario (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     primeiro_nome VARCHAR(255) NOT NULL,
@@ -17,5 +8,17 @@ CREATE TABLE IF NOT EXISTS usuario (
     login VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    data_criacao TIMESTAMP NOT NULL
+    data_criacao TIMESTAMP NOT NULL,
+    CONSTRAINT uk_email UNIQUE (email),
+    CONSTRAINT uk_login UNIQUE (login)
+);
+
+CREATE TABLE IF NOT EXISTS obra (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    autor BIGINT NOT NULL,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    status INT NOT NULL,
+    data_criacao TIMESTAMP NOT NULL,
+    CONSTRAINT fk_autor FOREIGN KEY (autor) REFERENCES usuario(id)
 );
